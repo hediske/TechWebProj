dc6e1f847f9b356f1e90527490fb78896493da1b
Brahim-Gdoura <brahim.gdoura2014@gmail.com>
2024-12-08 22:44:08 +0100

    product back-end : DB

--- back-end/src/product/product.service.ts
+++ back-end/src/product/product.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ProductEntity } from './entity/product.entity/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(
+        @InjectRepository(ProductEntity)
+        private productRepository: Repository<ProductEntity>
+    ){
+
+    }
+}
